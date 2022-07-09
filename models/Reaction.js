8055b94e6ec93c8d2955a9a0cc8500bccd67a6c6
RZhang <richardzhiyuanzhang@gmail.com>
2022-07-09 18:50:41 -0400

    created Reaction model, updated models/index.js

---
+++ models/Reaction.js
@@ -0,0 +1,48 @@
+const { Schema } = require('mongoose');
+const moment = require('moment');
+
+const reactionSchema = new Schema(
+  {
+
+    reactionID: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: [
+            true,
+            'Please enter a reaction'
+        ],
+        maxLength: [
+          280,
+          'Reaction must be less than or equal to 280 characters. Your reaction was {VALUE} characters long.'
+        ]
+    },
+    username: {
+        type: String,
+        required: [
+          true,
+          'There must be a username associated with this reaction.'
+      ]
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => moment(date).format('MMM Do, YYYY [at] hh:mm A')
+    }
+
+  },
+
+  {
+
+    toJSON: {
+      getters: true
+    },
+    id: false
+
+  }
+
+);
+
+module.exports = reactionSchema;
